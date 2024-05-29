bbca9fbbbb1ed894a2df3df30435e2e7f248f2ee
thailq <xinphatmutikill@gmail.com>
2024-05-29 20:32:15 +0700

    fix(medical-booking-forms): Automatically generate slug when empty

--- module/medical-booking-forms-management/modal-create-medical-booking-forms/index.tsx
+++ module/medical-booking-forms-management/modal-create-medical-booking-forms/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useState} from "react";
+import React from "react";
 import {Formik, FormikHelpers} from "formik";
 import {Col, Form, Row} from "antd";
 import FormItem from "@/components/FormItem";
@@ -7,9 +7,9 @@ import {FooterModalButton} from "@/components/ModalGlobal/FooterModalButton";
 import {useCreateMedicalBookingForms} from "@/utils/hooks/medical-booking-forms";
 import {InputGlobal} from "@/components/InputGlobal";
 import {
-  ICreateMedicalBookingForms,
-  RequiredMedicalBookingForms,
-  getValidationCreateMedicalBookingFormsSchema,
+   ICreateMedicalBookingForms,
+   RequiredMedicalBookingForms,
+   getValidationCreateMedicalBookingFormsSchema,
 } from "@/module/medical-booking-forms-management/modal-create-medical-booking-forms/form-config";
 import {useAppDispatch} from "@/redux/store";
 import {closeModal} from "@/redux/slices/ModalSlice";
@@ -18,110 +18,112 @@ import UploadImageGlobal from "@/components/UploadGlobal";
 import ApiUploadImage from "@/apiRequest/ApiUploadImage";
 
 export default function ContentModalCreateMedicalBookingForms({
-  refetch,
+   refetch,
 }: {
-  refetch: () => void;
+   refetch: () => void;
 }) {
-  const dispatch = useAppDispatch();
-  const {mutate: CreateMedicalBookingForms} = useCreateMedicalBookingForms();
+   const dispatch = useAppDispatch();
+   const {mutate: CreateMedicalBookingForms} = useCreateMedicalBookingForms();
 
-  const initialValues = {
-    name: "",
-    slug: "",
-    image: "",
-  };
+   const initialValues = {
+      name: "",
+      slug: "",
+      image: "",
+   };
 
-  const handleCreateMedicalBookingForms = async (
-    _values: ICreateMedicalBookingForms,
-    {setSubmitting}: FormikHelpers<RequiredMedicalBookingForms>,
-  ) => {
-    const formData = new FormData();
-    let values = _values;
-    const data = Boolean(values.slug)
-      ? values
-      : {
-          ...values,
-          slug: autoSlugify(values.name),
-        };
-    if (values?.image && typeof values.image !== "string") {
-      formData.append("image", values.image);
-      const imageUrl = await ApiUploadImage.uploadImage(formData);
-      values = {
-        ...data,
-        image: imageUrl.payload.data[0].url,
-      };
-    }
-    CreateMedicalBookingForms(
-      values as ICreateMedicalBookingForms & {slug: string},
-      {
-        onSuccess: () => {
-          dispatch(closeModal());
-          refetch();
-        },
-        onError: () => setSubmitting(false),
-      },
-    );
-  };
-  return (
-    <Formik
-      initialValues={initialValues}
-      validateOnBlur
-      validationSchema={getValidationCreateMedicalBookingFormsSchema()}
-      onSubmit={handleCreateMedicalBookingForms}
-    >
-      {({
-        isSubmitting,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        setFieldValue,
-      }): JSX.Element => (
-        <div className="modal-form-custom">
-          <Form onFinish={handleSubmit} labelAlign="left">
-            <Row gutter={24}>
-              <Col span={24}>
-                <div className="mb-5">
-                  <UploadImageGlobal
-                    url={initialValues.image}
-                    onChange={(file) => setFieldValue("image", file)}
+   const handleCreateMedicalBookingForms = async (
+      _values: ICreateMedicalBookingForms,
+      {setSubmitting}: FormikHelpers<RequiredMedicalBookingForms>,
+   ) => {
+      const formData = new FormData();
+      let values = _values;
+      const data = Boolean(values.slug)
+         ? values
+         : {
+              ...values,
+              slug: autoSlugify(values.name),
+           };
+      if (values?.image && typeof values.image !== "string") {
+         formData.append("image", values.image);
+         const imageUrl = await ApiUploadImage.uploadImage(formData);
+         values = {
+            ...data,
+            image: imageUrl.payload.data[0].url,
+         };
+      } else {
+         values = data;
+      }
+      CreateMedicalBookingForms(
+         values as ICreateMedicalBookingForms & {slug: string},
+         {
+            onSuccess: () => {
+               dispatch(closeModal());
+               refetch();
+            },
+            onError: () => setSubmitting(false),
+         },
+      );
+   };
+   return (
+      <Formik
+         initialValues={initialValues}
+         validateOnBlur
+         validationSchema={getValidationCreateMedicalBookingFormsSchema()}
+         onSubmit={handleCreateMedicalBookingForms}
+      >
+         {({
+            isSubmitting,
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            setFieldValue,
+         }): JSX.Element => (
+            <div className="modal-form-custom">
+               <Form onFinish={handleSubmit} labelAlign="left">
+                  <Row gutter={24}>
+                     <Col span={24}>
+                        <div className="mb-5">
+                           <UploadImageGlobal
+                              url={initialValues.image}
+                              onChange={(file) => setFieldValue("image", file)}
+                           />
+                        </div>
+                        <FormItem
+                           label="Tên danh mục"
+                           name="name"
+                           required
+                           labelCol={{span: 24}}
+                        >
+                           <InputGlobal
+                              name="name"
+                              placeholder="Nhập tên danh mục"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                           />
+                        </FormItem>
+                        <FormItem
+                           label="Slug"
+                           name="slug"
+                           required
+                           labelCol={{span: 24}}
+                        >
+                           <InputGlobal
+                              name="slug"
+                              placeholder="Nhập slug"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                           />
+                        </FormItem>
+                     </Col>
+                  </Row>
+                  <FooterModalButton
+                     textOk="Tạo hình thức đặt khám"
+                     onOk={handleSubmit}
+                     isLoading={isSubmitting}
                   />
-                </div>
-                <FormItem
-                  label="Tên danh mục"
-                  name="name"
-                  required
-                  labelCol={{span: 24}}
-                >
-                  <InputGlobal
-                    name="name"
-                    placeholder="Nhập tên danh mục"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                </FormItem>
-                <FormItem
-                  label="Slug"
-                  name="slug"
-                  required
-                  labelCol={{span: 24}}
-                >
-                  <InputGlobal
-                    name="slug"
-                    placeholder="Nhập slug"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                </FormItem>
-              </Col>
-            </Row>
-            <FooterModalButton
-              textOk="Tạo hình thức đặt khám"
-              onOk={handleSubmit}
-              isLoading={isSubmitting}
-            />
-          </Form>
-        </div>
-      )}
-    </Formik>
-  );
+               </Form>
+            </div>
+         )}
+      </Formik>
+   );
 }
--- module/medical-booking-forms-management/modal-edit-medical-booking-forms/index.tsx
+++ module/medical-booking-forms-management/modal-edit-medical-booking-forms/index.tsx
@@ -6,13 +6,13 @@ import FormItem from "@/components/FormItem";
 import {FooterModalButton} from "@/components/ModalGlobal/FooterModalButton";
 import {IModalProps} from "@/types";
 import {
-  useQueryGetMedicalBookingFormsById,
-  useUpdateMedicalBookingForms,
+   useQueryGetMedicalBookingFormsById,
+   useUpdateMedicalBookingForms,
 } from "@/utils/hooks/medical-booking-forms";
 import {InputGlobal} from "@/components/InputGlobal";
 import {
-  IEditMedicalBookingForms,
-  getValidationEditMedicalBookingFormsSchema,
+   IEditMedicalBookingForms,
+   getValidationEditMedicalBookingFormsSchema,
 } from "@/module/medical-booking-forms-management/modal-edit-medical-booking-forms/form-config";
 import {RequiredMedicalBookingForms} from "@/module/medical-booking-forms-management/modal-create-medical-booking-forms/form-config";
 import {useAppDispatch} from "@/redux/store";
@@ -20,128 +20,133 @@ import {closeModal} from "@/redux/slices/ModalSlice";
 import {autoSlugify} from "@/utils/constants/checkSlugify";
 import UploadImageGlobal from "@/components/UploadGlobal";
 import ApiUploadImage from "@/apiRequest/ApiUploadImage";
+import {useRouter} from "next/navigation";
 
 export default function ContentModalEditMedicalBookingForms(
-  props: IModalProps,
+   props: IModalProps,
 ) {
-  const dispatch = useAppDispatch();
-  const {data: medicalBookingForms} = useQueryGetMedicalBookingFormsById(
-    props.idSelect,
-  );
-  const {mutate: UpdateMedicalBookingForms} = useUpdateMedicalBookingForms();
+   const dispatch = useAppDispatch();
+   const {data: medicalBookingForms} = useQueryGetMedicalBookingFormsById(
+      props.idSelect,
+   );
+   const {mutate: UpdateMedicalBookingForms} = useUpdateMedicalBookingForms();
 
-  const initialValues = useMemo(() => {
-    return {
-      name: medicalBookingForms?.payload?.data.name || "",
-      slug: medicalBookingForms?.payload?.data.slug || "",
-      image: medicalBookingForms?.payload?.data.image || "",
-    };
-  }, [medicalBookingForms]);
-
-  const handleEditMedicalBookingForms = async (
-    _values: IEditMedicalBookingForms,
-    {setSubmitting}: FormikHelpers<RequiredMedicalBookingForms>,
-  ) => {
-    const formData = new FormData();
-    let values = _values;
-    if (initialValues?.image === values.image) {
-      formData.delete("image");
-    }
-    const data = Boolean(values.slug)
-      ? values
-      : {
-          ...values,
-          slug: autoSlugify(values.name),
-        };
-    if (values?.image && typeof values.image !== "string") {
-      formData.append("image", values.image);
-      const imageUrl = await ApiUploadImage.uploadImage(formData);
-      values = {
-        ...data,
-        image: imageUrl.payload.data[0].url,
+   const initialValues = useMemo(() => {
+      return {
+         name: medicalBookingForms?.payload?.data.name || "",
+         slug: medicalBookingForms?.payload?.data.slug || "",
+         image: medicalBookingForms?.payload?.data.image || "",
       };
-    }
-    UpdateMedicalBookingForms(
-      {
-        id: props.idSelect as string,
-        body: values as IEditMedicalBookingForms & {slug: string},
-      },
-      {
-        onSuccess: () => {
-          dispatch(closeModal());
-          if (props.refetch) {
-            props.refetch();
-          }
-        },
-        onError: () => setSubmitting(false),
-      },
-    );
-  };
-  if (!medicalBookingForms) return;
-  return (
-    <Formik
-      initialValues={initialValues}
-      validateOnBlur
-      validationSchema={getValidationEditMedicalBookingFormsSchema()}
-      onSubmit={handleEditMedicalBookingForms}
-    >
-      {({
-        isSubmitting,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        setFieldValue,
-      }): JSX.Element => (
-        <div className="modal-form-custom">
-          <Form onFinish={handleSubmit} labelAlign="left">
-            <Row gutter={24}>
-              <Col span={24}>
-                <div className="mb-5">
-                  <UploadImageGlobal
-                    url={values.image}
-                    onChange={(file) => setFieldValue("image", file)}
-                  />
-                </div>
+   }, [medicalBookingForms]);
 
-                <FormItem
-                  label="Tên danh mục"
-                  name="name"
-                  required
-                  labelCol={{span: 24}}
-                >
-                  <InputGlobal
-                    name="name"
-                    placeholder="Nhập tên danh mục"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
+   const handleEditMedicalBookingForms = async (
+      _values: IEditMedicalBookingForms,
+      {setSubmitting}: FormikHelpers<RequiredMedicalBookingForms>,
+   ) => {
+      const router = useRouter();
+      const formData = new FormData();
+      let values = _values;
+      if (initialValues?.image === values.image) {
+         formData.delete("image");
+      }
+      const data = Boolean(values.slug)
+         ? values
+         : {
+              ...values,
+              slug: autoSlugify(values.name),
+           };
+      if (values?.image && typeof values.image !== "string") {
+         formData.append("image", values.image);
+         const imageUrl = await ApiUploadImage.uploadImage(formData);
+         values = {
+            ...data,
+            image: imageUrl.payload.data[0].url,
+         };
+      } else {
+         values = data;
+      }
+      UpdateMedicalBookingForms(
+         {
+            id: props.idSelect as string,
+            body: values as IEditMedicalBookingForms & {slug: string},
+         },
+         {
+            onSuccess: () => {
+               dispatch(closeModal());
+               if (props.refetch) {
+                  props.refetch();
+               }
+               router.refresh();
+            },
+            onError: () => setSubmitting(false),
+         },
+      );
+   };
+   if (!medicalBookingForms) return;
+   return (
+      <Formik
+         initialValues={initialValues}
+         validateOnBlur
+         validationSchema={getValidationEditMedicalBookingFormsSchema()}
+         onSubmit={handleEditMedicalBookingForms}
+      >
+         {({
+            isSubmitting,
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            setFieldValue,
+         }): JSX.Element => (
+            <div className="modal-form-custom">
+               <Form onFinish={handleSubmit} labelAlign="left">
+                  <Row gutter={24}>
+                     <Col span={24}>
+                        <div className="mb-5">
+                           <UploadImageGlobal
+                              url={values.image}
+                              onChange={(file) => setFieldValue("image", file)}
+                           />
+                        </div>
+
+                        <FormItem
+                           label="Tên danh mục"
+                           name="name"
+                           required
+                           labelCol={{span: 24}}
+                        >
+                           <InputGlobal
+                              name="name"
+                              placeholder="Nhập tên danh mục"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.name}
+                           />
+                        </FormItem>
+                        <FormItem
+                           label="Slug"
+                           name="slug"
+                           required
+                           labelCol={{span: 24}}
+                        >
+                           <InputGlobal
+                              name="slug"
+                              placeholder="Nhập slug"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              value={values.slug}
+                           />
+                        </FormItem>
+                     </Col>
+                  </Row>
+                  <FooterModalButton
+                     textOk="Sửa hình thức đặt khám"
+                     onOk={handleSubmit}
+                     isLoading={isSubmitting}
                   />
-                </FormItem>
-                <FormItem
-                  label="Slug"
-                  name="slug"
-                  required
-                  labelCol={{span: 24}}
-                >
-                  <InputGlobal
-                    name="slug"
-                    placeholder="Nhập slug"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.slug}
-                  />
-                </FormItem>
-              </Col>
-            </Row>
-            <FooterModalButton
-              textOk="Sửa hình thức đặt khám"
-              onOk={handleSubmit}
-              isLoading={isSubmitting}
-            />
-          </Form>
-        </div>
-      )}
-    </Formik>
-  );
+               </Form>
+            </div>
+         )}
+      </Formik>
+   );
 }
